ef72e59dd152e33a25b00b661818951879b7b7eb
이경민 <lienero@naver.com>
2021-10-27 19:03:52 +0900

    컴포넌트 안에 다른 컴포넌트 사용하기 1

---
+++ vue-project/src/components/PageTitle.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- props에 정의된 키는 Vue 인스턴스의 데이터 값으로 사용되기 때문에 h2에서 이중 중괄호를 사용해서 -->
+  <!-- 문자열에 대한 데이터 바인딩 처리를 할 수 있습니다. -->
+  <h2>{{ title }}</h2>
+</template>
+<script>
+export default {
+  // props에는 부모 컴포넌트로 전달받은 데이터가 저장이 됩니다.
+  // props 키는 저장될 데이터의 타입과 부모 컴포넌트로부터 데이터가 전달되지 않았을 때의 default 값을 정의합니다.
+  props: {
+    title: {
+      type: String,
+      default: "페이지 제목입니다."
+    }
+  }
+};
+</script>
---
+++ vue-project/src/views/NestedComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <!-- 여기서 지정한 title 값이, 자식 컴포넌트인 PageTitle에 정의된 props의 title에 전달됩니다. -->
+    <PageTitle title="부모 컴포넌트에서 자식 컴포넌트로 데이터 전달" />
+  </div>
+</template>
+<script>
+// 컴포넌트 import
+import PageTitle from "../components/PageTitle";
+export default {
+  // 현재 컴포넌트에서 사용할 컴포넌트 등록
+  components: { PageTitle }
+};
+</script>
