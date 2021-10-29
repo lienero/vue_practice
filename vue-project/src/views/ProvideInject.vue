5f07a6de04a33cba2aee98b661ff0ed319ac59e2
이경민 <lienero@naver.com>
2021-10-29 19:45:19 +0900

    Provide/Inject 기능

---
+++ vue-project/src/views/ProvideInject.vue
@@ -0,0 +1,21 @@
+<template>
+  <ProvideInjectChild />
+</template>
+<script>
+import ProvideInjectChild from "./ProvideInjectChild";
+export default {
+  components: { ProvideInjectChild },
+  data() {
+    return {
+      items: ["A", "B"]
+    };
+  },
+  provide() {
+    return {
+      // Povide 함수를 통해서 배열 items의 length를 반환합니다.
+      // 자식 컴포넌트로 전달하고자 하는 데이터를 provide에 정의합니다.
+      itemLength: this.items.length
+    };
+  }
+};
+</script>
---
+++ vue-project/src/views/ProvideInjectChild.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  // 부모 컴포넌트로부터 전달받고자 하는 데이터와 동일한 속성 이르믕로 inject에 문자열 배열로 정의합니다.
+  // 아무리 계층 구조가 복잡해도 원하는 자식 컴포넌트로 데이터를 한 번에 전달할 수 있으나
+  // inject를 통해서 데이터를 전달받는 자식 컴포넌트는 어떤 부모 컴포넌트에서 전달되는 확인이 안되는 단점이 있다.
+  inject: ["itemLength"],
+  mounted() {
+    console.log(this.itemLength);
+  }
+};
+</script>
