8285496af5fb44fb0713859ba1904542c3231146
이경민 <lienero@naver.com>
2021-11-02 19:01:42 +0900

    컴포넌트API Provide/Inject

---
+++ vue-project/src/views/CompositionAPIInject.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>{{ title }}</h1>
+</template>
+<script>
+// inject 추가
+import { inject } from "vue";
+// js에서 계산과 관련된 외부 컴포넌트 불러오기
+export default {
+  setup() {
+    // 자식 컴포넌트에서는 inject를 이용해서 부모 컴포넌트에서 정의한 provide 키로 데이터를 가져올 수 있습니다.
+    const title = inject("title");
+    return { title };
+  }
+};
+</script>
---
+++ vue-project/src/views/CompositionAPIProvide.vue
@@ -0,0 +1,18 @@
+<template>
+  <CompositionAPIInject />
+</template>
+<script>
+// provide 추가
+import { provide } from "vue";
+// js에서 계산과 관련된 외부 컴포넌트 불러오기
+import CompositionAPIInject from "./CompositionAPIInject";
+export default {
+  components: {
+    CompositionAPIInject
+  },
+  setup() {
+    // provide 함수를 통해서 전달할 키(key), 값(value) 설정
+    provide("title", "Vue.js 프로젝트");
+  }
+};
+</script>
