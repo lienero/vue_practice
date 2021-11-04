1991d550d6aedeefb7fb7038b63475cf60cb79d6
이경민 <lienero@naver.com>
2021-11-04 19:42:58 +0900

    커스텀 디렉티브

---
+++ vue-project/src/views/CustomDirective.vue
@@ -0,0 +1,15 @@
+<template>
+  <input type="text" v-focus />
+</template>
+<script>
+export default {
+  directives: {
+    focus: {
+      mounted(el) {
+        // v-focus 디렉티브가 정의된 html 객체에 마우스 포커스가 위치하게 됩니다.
+        el.focus();
+      }
+    }
+  }
+};
+</script>
