e9c27c0d19aef8ed06066762d782caae7f52420b
이경민 <lienero@naver.com>
2021-11-12 19:28:21 +0900

    sweetalert2 설치

--- mini-project-shop/client/src/main.js
+++ mini-project-shop/client/src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue';
 import router from './router';
 import mixins from './mixins';
 import store from './store';
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
-createApp(App).use(store).use(router).mixin(mixins).mount('#app');
+createApp(App).use(store).use(router).use(VueSweetalert2).mixin(mixins).mount('#app');
 window.Kakao.init('64b8829af6e1aef65979542f5f196145');
